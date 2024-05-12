6ba920146087263696ee61b26ca3dd07bbb39559
Tanvir md al amin <01mdalamin1@gmail.com>
2024-05-12 06:21:45 +0600

    Start Shopify Theme development

---
+++ info.php
@@ -0,0 +1,24 @@
+<?php
+/*
+# https://www.udemy.com/course/shopify-theme-development-tailwindcss/learn/lecture/29740698#announcements
+# wooexperto@gmail.com | Tanvir12@#
+
+shopify theme init Dawn-clone | download Dawn theme
+shopify theme init virza --clone-url https://github.com/bbbpppolly/Elizabeth_Clean | download my github repojectory theme
+
+shopify theme dev --store vir-za.myshopify.com
+
+# npm init -y | create package.json
+# npm install -D tailwindcss | https://tailwindcss.com/docs/installation
+# npx tailwindcss init
+# npx tailwindcss -i ./src/tailwind.css -o ./assets/application.css --watch
+
+
+7. Section 7: JSON Templates 22 0:10/1:03 
+
+Tanvir+Afroza.9531805
+
+
+*/
+
+
