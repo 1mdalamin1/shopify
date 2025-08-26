8dbda95f8cf7f969baccd3f32b8a03e1473d8f17
Tanvir md al amin <01mdalamin1@gmail.com>
2025-08-26 11:19:39 +0600

    my 1st shopify theme done

--- shahriar-theme/info.php
+++
@@ -1,5 +0,0 @@
-<?php
-/**
- * This file is part of the Md. Shahriar Sizer. https://www.facebook.com/md.shahriar.sizer.98
- * I use his practice code for my own learning purposes and reduce the time to writing code.
-*/
