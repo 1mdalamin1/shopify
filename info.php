ea6e903b59130ec934866600266dca0bcc4a3159
Tanvir Hasan <47018365+virza805@users.noreply.github.com>
2025-08-27 22:31:18 +0600

    1st remix app start

--- info.php
+++ info.php
@@ -1,43 +1,17 @@
 <?php
 /*
 
-shopify theme init Dawn-clone | download Dawn theme
-shopify theme init virza --clone-url https://github.com/bbbpppolly/Elizabeth_Clean | download my github repojectory theme
+# https://www.shopifyacademy.com/path/developing-apps-for-shopify/app-development-fundamentals/2121032|=> To know more about Date Formatting
 
-shopify theme dev --store vir-za.myshopify.com
+# npm install -g @shopify/cli@latest |=> install shopify cli globally 
 
-# npm init -y | create package.json
-# npm install -D tailwindcss | https://tailwindcss.com/docs/installation
-# npx tailwindcss init
-# npx tailwindcss -i ./src/tailwind.css -o ./assets/application.css --watch
+# my 1st shopify app | Remix app
+> shopify app init --template remix | cd rrf-blank-theme
+> shopify app dev --store rrfv.myshopify.com
 
 
-7. Section 7: JSON Templates 30 0:10/37:31 
-
-
-# https://www.shopify.com/partners/blog/liquid-date-format |=> To know more about Date Formatting
-
-# 14:18s/19:42s শপিফাই - পর্ব ২৮ |=> https://www.youtube.com/watch?v=qfFbsRkR6EI&list=PLhPBqF--77InbheYB_rlgXSo-7DyECZTl&index=29
-
-* Shopify Theme Development start with RRF
-# https://shopify.dev/docs/api/shopify-cli
-> npm install -g @shopify/cli@latest
-> shopify help
-> shopify theme init
-> |=> reference-dawn-theme
-
-> shopify theme dev --store rrfv.myshopify.com
-> npm run dev | npm run build
-
-# my 1st theme
-> mkdir rrf-blank-theme | cd rrf-blank-theme
-> shopify theme dev --store rrfv.myshopify.com
-> npm run dev
-
-# shopify dummy product data => https://github.com/shopifypartners/product-csvs?tab=readme-ov-file
-# shopify dummy product data => https://drive.google.com/drive/folders/1O1HCyQR2n5gfgQgx1f5wdK9bhmTz2Lr9
 # https://heroicons.com |=> free svg icons
-
+# 05:18s/5:31s appRemix - 01 |=> https://www.youtube.com/watch?v=FMVShSvAaAA&list=PLB4AdipoHpxZu39dNjUWGRwgeKgW7mVJ-
 
 */
 
---
+++ my-shopify/info.php
@@ -0,0 +1,44 @@
+<?php
+/*
+
+shopify theme init Dawn-clone | download Dawn theme
+shopify theme init virza --clone-url https://github.com/bbbpppolly/Elizabeth_Clean | download my github repojectory theme
+
+shopify theme dev --store vir-za.myshopify.com
+
+# npm init -y | create package.json
+# npm install -D tailwindcss | https://tailwindcss.com/docs/installation
+# npx tailwindcss init
+# npx tailwindcss -i ./src/tailwind.css -o ./assets/application.css --watch
+
+
+7. Section 7: JSON Templates 30 0:10/37:31 
+
+
+# https://www.shopify.com/partners/blog/liquid-date-format |=> To know more about Date Formatting
+
+# 14:18s/19:42s শপিফাই - পর্ব ২৮ |=> https://www.youtube.com/watch?v=qfFbsRkR6EI&list=PLhPBqF--77InbheYB_rlgXSo-7DyECZTl&index=29
+
+* Shopify Theme Development start with RRF
+# https://shopify.dev/docs/api/shopify-cli
+> npm install -g @shopify/cli@latest
+> shopify help
+> shopify theme init
+> |=> reference-dawn-theme
+
+> shopify theme dev --store rrfv.myshopify.com
+> npm run dev | npm run build
+
+# my 1st theme
+> mkdir rrf-blank-theme | cd rrf-blank-theme
+> shopify theme dev --store rrfv.myshopify.com
+> npm run dev
+
+# shopify dummy product data => https://github.com/shopifypartners/product-csvs?tab=readme-ov-file
+# shopify dummy product data => https://drive.google.com/drive/folders/1O1HCyQR2n5gfgQgx1f5wdK9bhmTz2Lr9
+# https://heroicons.com |=> free svg icons
+
+npm install -g @shopify/cli@latest
+*/
+
+
