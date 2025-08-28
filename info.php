721f03fac36bc79794b1beddc24f9bd2bbdf1469
Tanvir Hasan <47018365+virza805@users.noreply.github.com>
2025-08-28 09:57:21 +0600

    create an app for shopify
    https://www.shopifyacademy.com/path/developing-apps-for-shopify/creating-an-app-for-shopify/2234068

--- info.php
+++ info.php
@@ -1,13 +1,23 @@
 <?php
 /*
 
-# https://www.shopifyacademy.com/path/developing-apps-for-shopify/app-development-fundamentals/2121032|=> To know more about Date Formatting
+# https://www.shopifyacademy.com/path/developing-apps-for-shopify |=> To know more about Date Formatting
 
 # npm install -g @shopify/cli@latest |=> install shopify cli globally 
 
+# https://www.shopifyacademy.com/path/developing-apps-for-shopify/app-development-fundamentals/2121032|=> To know more about Date Formatting
 # my 1st shopify app | Remix app
-> shopify app init --template remix | cd rrf-blank-theme
+> shopify app init --template remix | cd remix-app
+> shopify app generate extension |=> Extension-only apps can use metafields and metaobjects for basic data storage capabilities rather than hosting a server/db.
 > shopify app dev --store rrfv.myshopify.com
+> shopify app dev
+
+# https://www.shopifyacademy.com/path/developing-apps-for-shopify/creating-an-app-for-shopify/2234068 |=> To know more about Creating an app for shopify
+# my 1st admin-action app extension | Remix app
+> shopify app init | cd admin-action-app
+
+
+
 
 
 # https://heroicons.com |=> free svg icons
