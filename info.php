efd8c00789485d0b012a3334629f7f7769de4e02
Tanvir md al amin <01mdalamin1@gmail.com>
2025-08-28 15:44:10 +0600

    admin-action-app done

--- info.php
+++ info.php
@@ -13,15 +13,28 @@
 > shopify app dev
 
 # https://www.shopifyacademy.com/path/developing-apps-for-shopify/creating-an-app-for-shopify/2234068 |=> To know more about Creating an app for shopify
-# my 1st admin-action app extension | Remix app
+# my 1st admin-action app extension | extension-only apps can use metafields and metaobjects for basic data storage
 > shopify app init | cd admin-action-app
+> shopify app generate extension |=> Issue_Traker_Action
+> shopify app dev | shopify app dev --store rrfv.myshopify.com
+> shopify app build
+> shopify app deploy
+
+# https://www.shopifyacademy.com/path/developing-apps-for-shopify/app-surfaces-examples/2121090 |=> To know more about App Surfaces
+# Introduction to App Surfaces |
+
+# https://www.shopifyacademy.com/storefront-api-customization/1879615/scorm/bk6o4vb8yojy |=> To know more about Storefront API like metadata in wordpress.
+# Storefront API | Metaobjects | Metafields (metadata in wordpress)
+
 
 
 
 
 
 # https://heroicons.com |=> free svg icons
-# 05:18s/5:31s appRemix - 01 |=> https://www.youtube.com/watch?v=FMVShSvAaAA&list=PLB4AdipoHpxZu39dNjUWGRwgeKgW7mVJ-
+# 01:18s/9:54s appRemix - 02 |=> https://www.youtube.com/watch?v=vV1-gtUl2No&list=PLB4AdipoHpxZu39dNjUWGRwgeKgW7mVJ-&index=2
+
+
 
 */
 
