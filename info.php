f03ae2db03f5a871fe7a9e22f0f1a0524adaca7c
Tanvir md al amin <01mdalamin1@gmail.com>
2025-08-30 16:27:56 +0600

    add source code

--- info.php
+++ info.php
@@ -32,9 +32,43 @@
 
 
 # https://heroicons.com |=> free svg icons
-# 01:18s/9:54s appRemix - 02 |=> https://www.youtube.com/watch?v=vV1-gtUl2No&list=PLB4AdipoHpxZu39dNjUWGRwgeKgW7mVJ-&index=2
+# 01:18s/13:08s appRemix - 16 |=> https://www.youtube.com/watch?v=XqynXYAnbeY&list=PLB4AdipoHpxZu39dNjUWGRwgeKgW7mVJ-&index=16
+# shopify app | Buildify → Buildify Page Builder (and then you can say "The best page builder for Shopify" in your description) { main compititor is PageFly, Gempages, Shogun & Pagetify, Canva page builder, Insert page builder }
+
+# Buildify - The best page builder for Shopify
+> shopify app init --template remix | cd Buildify => The best page builder for Shopify
+> cd Buildify |=> shopify app generate extension |=> Extension-only apps can use metafields and metaobjects for basic data storage capabilities rather than hosting a server/db.
+> shopify app dev --store rrfv.myshopify.com
+> shopify backend ui |=> https://polaris-react.shopify.com/components
+> Remix doc |=> https://v2.remix.run/docs/discussion/data-flow/
+> Prisma ORM(Object-Relational Mapping) |=> https://www.prisma.io/ |=> Next-generation Node.js and TypeScript ORM || next-gen serverless Postgres database.
+> npm run prisma migrate dev -- --name create-settings-table |=> https://www.prisma.io/docs/getting-started/quickstart-sqlite
+
+> shopify app generate extension |=> https://shopify.dev/docs/apps/build/online-store/theme-app-extensions/build
+  > Theme app extensions |=> wishlist-icon |=> https://github.com/Hujjat/wishlist-inspire-app
+  > npm run deploy |=> https://shopify.dev/docs/apps/deployment
+
+  Alpine.js |=> https://alpinejs.dev/globals/alpine-data |=> A rugged, minimal framework for composing JavaScript behavior in your markup.
+
+> shopify app generate extension |=> Issue_Traker_Action
 
 
+# https://shopify.dev/docs/api/admin-graphql |=> To know more about Admin GraphQL API
+# https://shopify.dev/docs/api/shopify-app-remix/v2/apis/admin-api |=> To know more about Admin API
+
+
+> npm run prisma studio | it run http://localhost:5555/ |=> GUI to view and edit data in your database.
+
+> shopify app dev || npm run dev -- --theme [add_your_theme_id_here] |=> to see the changes in the store
+
+> 
+
+
+
+
+
+
+# 01:18s/25:08s appRemix - 23 |=> https://www.youtube.com/watch?v=1RQmp01vYNk&list=PLB4AdipoHpxZu39dNjUWGRwgeKgW7mVJ-&index=23
 
 */
 
